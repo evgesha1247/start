c04849927eecc16054ada550c065cd30d86e8093
evgesha1247 <dnsbtrq25@gmail.com>
2022-08-01 11:20:09 +0300

    стаедарный инкримент дикримент на провайдере

--- lib/ui/screens/home_screen/home_model_widget.dart
+++ lib/ui/screens/home_screen/home_model_widget.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/cupertino.dart';
 
-class HomeModelWidget extends ChangeNotifier {}
+class HomeModelWidget extends ChangeNotifier {
+  var _value = 0;
+  getValue() => _value.toString();
+
+  inc1() {
+    notifyListeners();
+    return (_value = _value + 1).toString();
+  }
+
+  dec1() {
+    notifyListeners();
+    return (_value = _value - 1).toString();
+  }
+}
--- lib/ui/screens/home_screen/home_screen_widget.dart
+++ lib/ui/screens/home_screen/home_screen_widget.dart
@@ -6,10 +6,36 @@ class HomeScreenWidget extends StatelessWidget {
   const HomeScreenWidget({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final model = context.watch<HomeModelWidget>();
+    final model = context.read<HomeModelWidget>();
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text('----')),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: model.inc1,
+              child: const Icon(Icons.plus_one),
+            ),
+            const ValueWidget(),
+            ElevatedButton(
+              onPressed: model.dec1,
+              child: const Icon(Icons.exposure_minus_1),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ValueWidget extends StatelessWidget {
+  const ValueWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final value = context.watch<HomeModelWidget>().getValue();
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: Text(value),
     );
   }
 }
